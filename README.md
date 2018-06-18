8016f576f1ad28c4120d490dcc9f94288112c88c
Rafael Nojiri <peo_rnojiri@uolinc.com>
2018-06-18 17:44:03 -0300

    Primeiro commit.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# teste
+# Como executar
+
+Clone esse repositório com:
+```sh
+git clone https://github.com/rnojiri/teste.git
+```
+Execute com:
+```sh
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,8 @@
+//importa a lib que pega o input do usuario
+var prompt = require('prompt-sync')();
+
+//faz alguma pergunta e pega o valor do usuario
+var valor = prompt('Pergunta? ');
+
+//printa o valor
+console.log(valor);
